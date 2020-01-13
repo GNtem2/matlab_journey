e91e8b03972ca7c7aa14b13938d4975918fdc421
GNtem2 <thanh.phan@monash.edu>
2020-01-13 13:41:41 +1100

    Add files via upload

--- tsne_image.m
+++ tsne_image.m
@@ -46,34 +46,70 @@ Vol=categorical(Vol) %convert to categorical
 
 %combine data into array
 ArrayCentroid=[a1;b1;c1]' %transpose to create 3 columns
-ArrayCentroid=[ArrayCentroid;Vol]
+ArrayCentroid=[ArrayCentroid;Vol] %error combining doubles and categorical variables
+ArrayCentroidTable=table(ArrayCentroid,categorical(Vol))
 
+%%%
+ 10×2 table
+
+          ArrayCentroid            Var2 
+    __________________________    ______
+
+    72.005    68.601    57.502    small 
+    71.005    70.601    69.502    medium
+    66.005    75.601    63.502    large 
+    67.005    73.601    68.502    large 
+    64.005    72.601    70.502    medium
+    65.005    65.601    64.502    medium
+    63.005    69.601    66.502    large 
+    70.005    64.601    62.502    large 
+    68.005    71.601    58.502    small 
+    69.005    63.601    67.502    medium
+
+ %access table   
+ ArrayCentroidTable.ArrayCentroid
+ ArrayCentroidTable.Var2
+ %change column names
+ ArrayCentroidTable.Properties.VariableNames={'XYZdim','Volume'}
+
+ 
+%%%
 %3d scatter plot
 scatter3(a1',b1',c1')
 %alternate
 scatter3(ArrayCentroid(:,1),ArrayCentroid(:,2),ArrayCentroid(:,3))
 %add color by vector Vol
 scatter3(ArrayCentroid(:,1),ArrayCentroid(:,2),ArrayCentroid(:,3),[],Vol)
+%plotting by table
+scatter3(ArrayCentroidTable.XYZdim(:,1),ArrayCentroidTable.XYZdim(:,2),ArrayCentroidTable.XYZdim(:,3),[},ArrayCentroidTable.Volume)
+
+
 
 %create 4 subplot
 rng default % for reproducibility
-Y = tsne(ArrayCentroid(:,1:3),'Algorithm','exact','Distance','euclidean');
+Y = tsne(ArrayCentroidTable.XYZdim,'Algorithm','exact','Distance','euclidean');
 subplot(2,2,1)
-gscatter(Y(:,1),Y(:,2),Vol) %scater plot
+gscatter(Y(:,1),Y(:,2),ArrayCentroidTable.Volume) %scater plot
 title('Euclidean')
 
-Y = tsne(ArrayCentroid,'Algorithm','exact','Distance','mahalanobis');
+Y = tsne(ArrayCentroidTable.XYZdim,'Algorithm','exact','Distance','mahalanobis');
 subplot(2,2,2)
-gscatter(Y(:,1),Y(:,2),Vol) %scater plot 
+gscatter(Y(:,1),Y(:,2),ArrayCentroidTable.Volume) %scater plot 
 title('Mahalanobis')
 
-Y = tsne(ArrayCentroid,'Algorithm','exact','Distance','chebychev');
+Y = tsne(ArrayCentroidTable.XYZdim,'Algorithm','exact','Distance','chebychev');
 subplot(2,2,3)
-gscatter(Y(:,1),Y(:,2),Vol) %scater plot
+gscatter(Y(:,1),Y(:,2),ArrayCentroidTable.Volume) %scater plot
 title('Chebychev')
 
 Y = tsne(ArrayCentroid,'Algorithm','exact','Distance','cosine');
 subplot(2,2,4)
-gscatter(Y(:,1),Y(:,2),Vol) %scater plot
+gscatter(Y(:,1),Y(:,2),ArrayCentroidTable.Volume) %scater plot
 title('Cosine')
 
+%Add tsne output
+ArrayCentroidTable=table(ArrayCentroidTable,Y(:,1),Y(:,2))
+ArrayCentroidTable.Properties.VariableNames={'XYZdim','TSNEdim1','TSNEdim2'}
+
+%save output to CVS - cannot handle nested table
+%writetable(ArrayCentroidTable,'SimulatedCentroid.csv')
